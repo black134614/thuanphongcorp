493d2f62b33e88db442b56a586f4e1ca90a525de
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2023-09-16 16:38:05 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './components/home/home.css'
 import Loading from './loading'
 import { Suspense } from 'react'
 import Facebook from './components/Facebook'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Thuận Phong Corp',
@@ -27,6 +28,27 @@ export default function RootLayout({
         </Suspense>
         <div id='fb-root'></div>
         <div id='fb-customer-chat' className='fb-customerchat'></div>
+        <Script id='chatfb'>
+          {`var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "537686649638747");
+    chatbox.setAttribute("attribution", "biz_inbox");`}
+        </Script>
+        <Script id='addPlugin'>
+          {` window.fbAsyncInit = function() {
+        FB.init({
+        xfbml            : true,
+        version          : 'v18.0'
+        });
+        };
+        
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));`}
+        </Script>
         {/* <Facebook /> */}
       </body>
     </html>
