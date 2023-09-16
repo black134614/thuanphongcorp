857d80a402dd80a92013e0e995dfa3915301e863
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2023-09-16 15:42:56 +0700

    add zalo oa

--- pages/_app.js
+++ pages/_app.js
@@ -4,5 +4,10 @@ config.autoAddCss = false
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+
+    <Component {...pageProps} />
+    <div className="zalo-chat-widget" data-oaid="1337303147334772731" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height=""></div>
+    <script src="https://sp.zalo.me/plugins/sdk.js"></script>
+  </>
 }
