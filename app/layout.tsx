cdc60b5e82bbb1d574853390728557d32a2a96e4
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2023-09-16 16:19:13 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </Suspense>
-        <Facebook />
+        {/* <Facebook /> */}
       </body>
     </html>
   )
