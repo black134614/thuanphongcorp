034201c6d1f4a75447d7311704cc31d00fb22b38
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2023-09-16 15:30:27 +0700

    .

--- app/du-an/page.tsx
+++ app/du-an/page.tsx
@@ -1,14 +1,7 @@
-'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 export default function page() {
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    })
-  }, [])
-
+  
   return (
     <div className='min-h-screen bg-gray-900 flex flex-col items-center justify-center'>
       <h1 className='text-5xl text-white font-bold mb-8 animate-pulse'>
